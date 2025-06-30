f35dcba7bc4c2d3b940ebd70a0e6cabd560ca935
william.gomes <wrgomes1991@gmail.com>
2025-06-29 21:08:51 -0300

    add plan

--- Models/proposal.py
+++ Models/proposal.py
@@ -1,17 +1,22 @@
 from datetime import datetime
 
 class Proposal:
-    def __init__(self, cpf: str, name: str, age:int, proposal_time:datetime):
+    def __init__(self, cpf: str, name: str, age:int, proposal_time:datetime, plan:str):
         self.cpf = cpf
         self.name = name
         self.age = age
-        # self.plan = plan
+        self.plan = plan
         # self.coverage_amount = coverage_amount
         # self.monthly_premium = monthly_premium
         # self.city = city    
         # self.claim_history = claim_history
         self.proposal_time = proposal_time
+    def __result__(self, plan:str, proposal_time:datetime):
+        self.history.append(Proposal)
+        
+
+    
 
     def __repr__(self):
-        return f"Proposal(cpf={self.cpf}, name={self.name}, age={self.age})"
+        return f"Proposal(plan={self.plan}, name={self.name}, age={self.age}, Date={self.proposal_time})"
         
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from Models.proposal import Proposal
 from service.validate_proposal import validate_proposal
 
 if __name__ == "__main__":
-    proposal = Proposal(cpf="38452398832", name="Arico", age=30, proposal_time=datetime.now())
+    proposal = Proposal(cpf="14752353832", plan="S750 Amil", name="Arico", age=30, proposal_time=datetime.now())
 
     status = validate_proposal(proposal)
     print(f"Status: {status}")
+    print(proposal)
 
